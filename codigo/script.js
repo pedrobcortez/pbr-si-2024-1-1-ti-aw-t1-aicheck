cc06dc9434908c358117cf927106de8af02df38d
pedrobcortez <72892962+pedrobcortez@users.noreply.github.com>
2024-06-30 00:32:03 -0300

    Create script.js

---
+++ codigo/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var form = document.getElementById("loginForm");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita o envio do formulário padrão
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+  // Validação do email
+    var emailRegex = /@gmail\.com$|@hotmail\.com$|@yahoo\.com$|@outlook\.com$/;
+    var isEmailValid = emailRegex.test(email);
+
+    // Validação da senha
+    var passwordRegex = /^(?=.*[A-Z]).{6,}$/;
+    var isPasswordValid = passwordRegex.test(password);
+
+    // Verifica se o email e a senha são válidos
+    if (isEmailValid && isPasswordValid) {
+      // Se as credenciais estiverem corretas, redirecione para outra página
+      window.location.href = "pagina_eventos.html";
+    } else {
+      // Caso contrário, exiba uma mensagem de erro
+      alert("Senha deve ter mais de 6 caracteres e com caractere maiúscula");
+    }
+  });
+});
